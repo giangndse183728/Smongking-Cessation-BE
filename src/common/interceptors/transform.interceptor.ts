95e0f2515d281c4139270d97073072a1b0235c43
giangndse183728 <giangndse183728@fpt.edu.vn>
2025-05-19 21:35:33 +0700

    initial commit

---
+++ src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface Response<T> {
+  statusCode: number;
+  timestamp: string;
+  msg: string;
+  data: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+   
+
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: response.statusCode,
+        msg: 'Success!',
+        data,
+        timestamp: new Date().toISOString(),
+      })),
+    );
+  }
+}
